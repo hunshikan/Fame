83053090f4a4154f41d4b2e82d2973784eaa2e46
ZBW <920049380@qq.com>
2017-08-07 21:43:10 +0800

    环境集成
    添加spring-boot+mybatis+pagehelper+mapper集成环境

---
+++ src/main/java/com/zbw/fame/controller/admin/ArticleController.java
@@ -0,0 +1,17 @@
+package com.zbw.fame.controller.admin;
+
+import com.zbw.fame.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 后台文章管理 Controller
+ *
+ * @auther zbw
+ * @create 2017/7/11 19:52
+ */
+@Controller
+@RequestMapping("admin/article")
+public class ArticleController extends BaseController{
+
+}
---
+++ src/main/java/com/zbw/fame/mapper/ContentsMapper.java
@@ -0,0 +1,13 @@
+package com.zbw.fame.mapper;
+
+import com.zbw.fame.model.Contents;
+import com.zbw.fame.util.MyMapper;
+
+/**
+ * Contents Mapper
+ *
+ * @auther zbw
+ * @create 2017/7/8 10:39
+ */
+public interface ContentsMapper extends MyMapper<Contents>{
+}
